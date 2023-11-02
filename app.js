3277b717b6d386454c296a23c190e0661c70b680
YlanNaly <hei.nalisoa@gmail.com>
2023-11-02 10:04:23 +0300

    feat: dir emplacement

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
 const express = require('express');
-const app = express();
+const server = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const logger = require('winston');
 const cors = require('cors'); // Import the 'cors' package
 
-const allowedOrigins = ['*'];
-var cors_op =function (req, callback) {
-    var corsOptions;
-    if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
-      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
-    } else {
-      corsOptions = { origin: false } // disable CORS for this request
-    }
-    callback(null, corsOptions) // callback expects two parameters: error and options
-  }
-// Serve the static website from the 'static-website' directory
-app.use(express.static(path.join(__dirname, 'static-website')));
- // You can use a logging library like 'winston'
+server.use(cors()); // Allow cross-origin requests
+server.use(express.static(path.join(__dirname, 'static-website')));
 
-// Configure the logger
 logger.add(new logger.transports.File({ filename: 'login_log.txt' }));
 logger.level = 'info';
 
-app.use(bodyParser.json());
-
-app.post('/login', cors(cors_op), (req, res) => {
+server.use(bodyParser.json());
+server.post('/login', (req, res) => {
   try {
     const { cliId, username, password } = req.body;
 
@@ -47,7 +34,7 @@ app.post('/login', cors(cors_op), (req, res) => {
   }
 });
 
-app.all('/login', cors(cors_op), (req, res) => {
+server.all('/login', (req, res) => {
   const { cliId, username, password } = req.body;
 
   const request_data_to_log = {
@@ -63,6 +50,6 @@ app.all('/login', cors(cors_op), (req, res) => {
   res.status(405).json({ message: 'Invalid request method' });
 });
 
-app.listen(8080, () => {
+server.listen(8000, () => {
   console.log('Express server is running on port 8080');
-});
+})
