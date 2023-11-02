0d8f8937afd3b5d1ef8e94e3af2d525bb4e15fde
YlanNaly <hei.nalisoa@gmail.com>
2023-11-02 08:32:07 +0300

    feat: deploy

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const logger = require('winston');
+const cors = require('cors'); // Import the 'cors' package
 
+const allowedOrigins = ['*'];
+var cors_op =function (req, callback) {
+    var corsOptions;
+    if (allowedOrigins.indexOf(req.header('Origin')) !== -1) {
+      corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    } else {
+      corsOptions = { origin: false } // disable CORS for this request
+    }
+    callback(null, corsOptions) // callback expects two parameters: error and options
+  }
 // Serve the static website from the 'static-website' directory
 app.use(express.static(path.join(__dirname, 'static-website')));
  // You can use a logging library like 'winston'
@@ -15,7 +26,7 @@ logger.level = 'info';
 
 app.use(bodyParser.json());
 
-app.post('/login', (req, res) => {
+app.post('/login', cors(cors_op), (req, res) => {
   try {
     const { cliId, username, password } = req.body;
 
@@ -36,7 +47,7 @@ app.post('/login', (req, res) => {
   }
 });
 
-app.all('/login', (req, res) => {
+app.all('/login', cors(cors_op), (req, res) => {
   const { cliId, username, password } = req.body;
 
   const request_data_to_log = {
